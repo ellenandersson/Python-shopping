2576ff0e0b2afc014d0e9db0c55cd8a950f90847
Ellen <ellen.andersson89@gmail.com>
2025-05-02 12:39:19 +0200

    using the .env

--- src/config.py
+++ src/config.py
@@ -1,11 +1,14 @@
+import os
+from dotenv import load_dotenv
+
 BASE_URL = "https://www.popmart.com/se"
 LOGIN_URL = f"{BASE_URL}/user/login"
 CART_URL = f"{BASE_URL}/largeShoppingCart"
 CHECKOUT_URL = f"{BASE_URL}/checkout"
 PRODUCT_URL = f"{BASE_URL}/products/1443/BAZBON-Label-Plan-Series-1%2F8-Action-Figure"
 
-USERNAME = "your_username"
-PASSWORD = "your_password"
+USERNAME = os.getenv("USERNAME", "fake")
+PASSWORD = os.getenv("PASSWORD", "fake")
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
